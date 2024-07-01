79c91966f8122a34a10969a0efff0cb66e5c154f
Oliver Holl <oholl@ethz.ch>
2024-07-01 16:54:43 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    watch: {
+      usePolling: true, // Helps with some file system watchers, especially on Windows
+    },
+  },
 });
