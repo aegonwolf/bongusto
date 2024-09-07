5f2e095ec3e409ee6361df7649ba59a090b639a0
Oliver Holl <oholl@ethz.ch>
2024-09-07 10:33:39 +0200

    secret

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,11 +1,13 @@
 // Copy and paste the validateToken function from above here
 
+import { TURNSTILEKEY } from '$env/static/private';
+
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
 
         const token = data.get('cf-turnstile-response'); // if you edited the formsField option change this
-        const SECRET_KEY = '...'; // you should use $env module for secrets
+        const SECRET_KEY = TURNSTILEKEY;
 
         const { success, error } = await validateToken(token, SECRET_KEY);
 
